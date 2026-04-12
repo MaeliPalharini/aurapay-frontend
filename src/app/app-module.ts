ccaf10d138b9256855d54e4a53893dc5915bca41
Maeli <maeli.palharini@hotmail.com>
2026-04-11 21:21:09 -0300

    feat: Update navigation and add piggy bank pages with enhanced styles

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,5 +1,6 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
@@ -7,15 +8,20 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { CreateCustomerPage } from './features/customers/pages/create-customer-page/create-customer-page';
 import { WalletDetailsPage } from './features/wallet/pages/wallet-details-page/wallet-details-page';
+import { PiggyBankPage } from './features/piggy-banks/pages/piggy-bank-page/piggy-bank-page';
+import { PiggyBankDetailsPage } from './features/piggy-banks/pages/piggy-bank-details-page/piggy-bank-details-page';
 
 @NgModule({
   declarations: [
     App,
     CreateCustomerPage,
-    WalletDetailsPage
+    WalletDetailsPage,
+    PiggyBankPage,
+    PiggyBankDetailsPage
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     ReactiveFormsModule
   ],
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateCustomerPage } from './features/customers/pages/create-customer-page/create-customer-page';
 import { WalletDetailsPage } from './features/wallet/pages/wallet-details-page/wallet-details-page';
+import { PiggyBankPage } from './features/piggy-banks/pages/piggy-bank-page/piggy-bank-page';
+import { PiggyBankDetailsPage } from './features/piggy-banks/pages/piggy-bank-details-page/piggy-bank-details-page';
 
 const routes: Routes = [
   { path: '', redirectTo: 'customers/new', pathMatch: 'full' },
   { path: 'customers/new', component: CreateCustomerPage },
   { path: 'wallet', component: WalletDetailsPage },
+  { path: 'cofrinhos', component: PiggyBankPage },
+  { path: 'cofrinhos/:id', component: PiggyBankDetailsPage },
+  { path: 'deposit', redirectTo: 'cofrinhos', pathMatch: 'full' },
 ];
 
 @NgModule({
